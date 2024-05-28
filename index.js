9c8bf0c07e342bea1069eb90f221776fb5f5381c
Yusuf Shah <yusufshahdrive@gmail.com>
2024-05-28 21:54:21 +0530

    added parralax

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showProject');
         }
     });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements2 = document.querySelectorAll('.hiddenCard');
 hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements2.forEach((el) => observer2.observe(el));
+
+window.addEventListener('scroll', function() {
+    var scrolled = window.scrollY;
+    var background = document.querySelector('.background');
+    background.style.transform = 'translateY(' + -(scrolled * 0.5) + 'px)';
+});
